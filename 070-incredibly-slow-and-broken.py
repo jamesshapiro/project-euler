025a1c9d134e206f49c7f71f595179ecc38f7bb4
James Shapiro <jamesshapiro@gmail.com>
2017-07-19 19:24:35 -0700

    still broken

--- 070-incredibly-slow-and-broken.py
+++ 070-incredibly-slow-and-broken.py
@@ -3,6 +3,9 @@
 import naive_prime_test as t
 import numpy as np
 
+prime_divisors = {}
+
+
 def list_digits(x):
     return "".join(sorted(str(x)))
 
@@ -11,33 +14,43 @@ def have_same_digits(x, y):
 
 primes = [x for x in range(1,10000000//3 + 1) if t.is_prime(x)]
 
-def prime_divisors(n):
+print("got primes")
+
+def get_prime_divisors(n):
     results = []
     n_copy = n
     for prime in primes:
         if n % prime == 0:
             results.append(prime)
             n_copy = n_copy // prime
-        if n_copy < prime:
-            break
-    return results
+            results.extend(prime_divisors[n_copy])
+    prime_divisors[n] = results
+
+prime_divisors[1] = []
+for i in range(2, 10000000):
+    get_prime_divisors(i)
+
+print("got prime divisors")
 
 euler_totients = []
-
 def euler_totient(n):
     totient = n
-    for prime in prime_divisors(n):
-        totient = totient // prime
-        totient *= prime - 1
-    if totient == n:
+    if t.is_prime(n):
         totient = n - 1
+    else:
+        for prime in prime_divisors[n]:
+            totient = totient // prime
+            totient *= prime - 1
     euler_totients.append((n, totient))
     return (n, totient)
+
 
 for i in range(2,10000000):
     print(i)
     euler_totient(i)
     (x,y) = euler_totients[i-2]
+
+print("got euler totients")
 
 for et in range(len(euler_totients)):
     (x,y) = euler_totients[et]
