f7c252a8d9acfe41b2dc7f3b00e79ea23a7678a1
James Shapiro <jamesshapiro@gmail.com>
2017-07-30 22:55:13 -0700

    rewrite 030 and 032

--- 030.py
+++ 030.py
@@ -2,8 +2,8 @@
 
 fifth_powers = {x: x**5 for x in range(10)}
 
-def is_digit_fifth(x):
+def is_sum_of_its_digits_to_the_fifth(x):
     return int(x) == sum(fifth_powers[int(i)] for i in x) and int(x) > 1
 
-print(sum(i for i in range(1000000) if is_digit_fifth(str(i))))
+print(sum(i for i in range(1000000) if is_sum_of_its_digits_to_the_fifth(str(i))))
         
--- 032.py
+++ 032.py
@@ -2,13 +2,17 @@
 
 import math
 
-string = "123456789"
+digits = "123456789"
 
-def is_pandigital(x):
+def is_pandigital(string):
+    return "".join(sorted(string)) == digits
+
+def is_pandigital_product(x):
     for i in range(1, math.ceil(math.sqrt(x)) + 1):
         if x % i == 0:
-            if "".join(sorted(str(x//i) + str(i) + str(x))) == string:
+            equation_as_string = str(i) + str(x//i) + str(x)
+            if is_pandigital(equation_as_string):
                 return True
     return False
 
-print(sum(x for x in range(10000) if is_pandigital(x)))
+print(sum(x for x in range(10000) if is_pandigital_product(x)))
