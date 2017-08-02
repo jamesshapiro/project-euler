b0698257989768773ab5c4b3de4a0fbc2cb7e928
James Shapiro <jamesshapiro@gmail.com>
2017-08-01 23:11:56 -0700

    make problem 39 easier to understand by associating perimeters with the number of solutions rather than implicitly storing the perimeters as indices to an array

--- 038.py
+++ 038.py
@@ -5,7 +5,11 @@ digits = "123456789"
 def is_pandigital(x):
     return "".join(sorted(x)) == digits
 
+"""                  "192" "384" "576"
+                  192*1   192*2   192*3
+"""
 largest_pandigital = 192384576
+
 for i in range(1, 500000):
     digit_str = ""
     j = 1
--- 039.py
+++ 039.py
@@ -2,12 +2,12 @@
 
 import math
 import itertools
-import numpy as np
+import operator
 
 def is_solution(leg_1, leg_2, hypotenuse):
     return leg_1**2 + leg_2**2 == hypotenuse**2
 
-num_solutions = [0, 0]
+num_solutions = {1: 0, 2: 0}
 
 for perimeter in range(3, 1001):
     solutions = set()
@@ -18,7 +18,7 @@ for perimeter in range(3, 1001):
                 continue
             if is_solution(leg_1, leg_2, hypotenuse):
                 solutions.add((leg_1, leg_2, hypotenuse))
-    num_solutions.append(len(solutions))
-    
-arr = np.array(num_solutions)
-print(np.argmax(arr) + 1)
+    num_solutions[perimeter] = len(solutions)
+
+perimeter_with_most_solutions = max(num_solutions.keys(), key=(lambda key: num_solutions[key]))
+print(perimeter_with_most_solutions)
