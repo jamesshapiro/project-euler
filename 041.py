ee09bd5efadce9611d81bd8722c4f52fb7228633
James Shapiro <jamesshapiro@gmail.com>
2017-08-02 20:00:00 -0600

    simplify problem 41

--- 041.py
+++ 041.py
@@ -2,25 +2,16 @@
 
 import math
 import itertools
+import naive_prime_test as t
 
 digits = "123456789"
 
-def is_prime(x):
-    if x == 1:
-        return False
-    if x == 2:
-        return True
-    for i in range(2, math.ceil(math.sqrt(x)) + 1):
-        if x % i == 0:
-            return False
-    return True
-
 pandigital_primes = []
-subsets = [digits[:i+1] for i in range(len(digits))]
-pandigital_subsets = [itertools.permutations(subset,len(subset)) for subset in subsets]
+prefixes = [digits[:i+1] for i in range(len(digits))]
+pandigital_subsets = [itertools.permutations(subset) for subset in prefixes]
 for subset in pandigital_subsets:
     numbers = map(lambda x: int("".join(x)), subset)
-    pandigital_primes.extend(filter(is_prime, numbers))
+    pandigital_primes.extend(filter(t.is_prime, numbers))
 
 print(max(pandigital_primes))
 
